1531795abcb44fde07cf08a1eb29077bc60d4c1a
AbhinavReddy-git <naredlaabhinavreddy@gmail.com>
2026-08-15 22:02:22 +0530

    reviewing the resume

--- readResume.js
+++ readResume.js
@@ -3,11 +3,23 @@ import { parse } from "path";
 import {PDFParse} from "pdf-parse";
 const pdfBuffer=fs.readFileSync("./resumes/resume.pdf");
 
+const requiredSkills=["JavaScript","Python","AWS","React","Node.js","Mongo DB"];
 
 async function readResume() {
   const parser = new PDFParse({data:pdfBuffer});
   const result = await parser.getText();
-  console.log(result.text);
+
+  const resumeText = result.text.toLowerCase();
+
+  const matchedSkills = requiredSkills.filter(skills=> resumeText.includes(skills.toLowerCase()));
+  const missingSkills = requiredSkills.filter(skills => !resumeText.includes(skills.toLowerCase()));
+  const matchPercentage = (matchedSkills.length/requiredSkills.length)*100;
+
+  console.log("===AI RESUME ANALYZER");
+  console.log("Matched : ",matchedSkills);
+  console.log("Missing : ",missingSkills);
+  console.log("Match : ",matchPercentage+"%");
+
   await parser.destroy();
 }
 
